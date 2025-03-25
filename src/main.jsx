88565af25bd516f419fd7188e54e370165535ef8
Antonin Riviere <antonin.rvr@gmail.com>
2025-03-25 12:42:22 +0100

    :sparkles: starter > add drei and orbit controls

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import './style.css'
 import { createRoot } from 'react-dom/client'
 import Experience from './Experience.jsx'
 import { Canvas } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
 
 createRoot(document.getElementById('root')).render(
     <Canvas>
         <Experience />
+        <OrbitControls />
     </Canvas>
 )
